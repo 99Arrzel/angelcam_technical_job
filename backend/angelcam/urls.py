1bbc18514801950c95e453e6186075c117836831
Andres Carrillo <af.carrillo@live.com>
2024-08-05 06:10:16 -0400

    Add path of shared cameras

--- backend/angelcam/urls.py
+++ backend/angelcam/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from angelcam.endpoints.landing import getData
 from angelcam.endpoints.auth import auth_me
+from angelcam.endpoints.my_cameras import shared_cameras
 from django.conf import settings
 
 urlpatterns = [
   path('', getData),
   path('auth', auth_me), 
+  path('shared-cameras', shared_cameras),
 ]
